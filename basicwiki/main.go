04d5c3875ab571be945eeebb9e4f9c668c67207c
P. Todd Decker <ptdecker@mac.com>
2017-10-01 16:42:11 -0500

    Enhanced to provide basic web server to serve wiki pages

--- basicwiki/main.go
+++ basicwiki/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"fmt"
+	"net/http"
 )
 
 // Page structure
@@ -30,11 +31,17 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+// View pages web handler
+
+func viewHandler(w http.ResponseWriter, r *http.Request)  {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 // Main
 
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	http.ListenAndServe(":8080", nil)
 }
